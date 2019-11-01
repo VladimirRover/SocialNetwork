d052cba7d428c593d4f9e1907d2e92530bffc1b3
vakhtang <sogomonyan@alliance-electro.ru>
2019-11-01 17:53:10 +0300

    migration issue resolved tests passed

--- src/test/resources/create-user-before.sql
+++ src/test/resources/create-user-before.sql
@@ -1,9 +1,9 @@
 delete from user_role;
 delete from usr;
 
-insert into usr(active, password, username) values
-(true, '$2a$08$iXqFMQPEzd9Oo9GjFe8AU.pwIStIwmKLGE4Q7xanv0teGWkue6.Jy', 'admin'),
-(true, '$2a$08$iXqFMQPEzd9Oo9GjFe8AU.pwIStIwmKLGE4Q7xanv0teGWkue6.Jy', 'user');
+insert into usr(id, active, password, username) values
+(1, true, '$2a$08$iXqFMQPEzd9Oo9GjFe8AU.pwIStIwmKLGE4Q7xanv0teGWkue6.Jy', 'admin'),
+(2, true, '$2a$08$iXqFMQPEzd9Oo9GjFe8AU.pwIStIwmKLGE4Q7xanv0teGWkue6.Jy', 'user');
 
 insert into user_role(user_id, roles) values
 (1, 'USER'),
--- src/test/resources/messages-list-before.sql
+++ src/test/resources/messages-list-before.sql
@@ -2,8 +2,8 @@ delete from message;
 
 insert into message(id, text, tag, user_id) values
 (1, 'first', 'tag', 2),
-(1, 'second', 'tag', 2),
-(1, 'third', 'tag', 2),
-(1, 'fourth', 'tag', 2);
+(2, 'second', 'tag', 2),
+(3, 'third', 'tag', 2),
+(4, 'fourth', 'tag', 2);
 
 alter sequence hibernate_sequence restart with 10;
